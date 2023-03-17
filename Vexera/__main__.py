09d708eb1ca4b564a6b76c1a9815b8dba58c25d6
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2023-03-17 20:44:01 +0530

    Vexera V-1

---
+++ Vexera/__main__.py
@@ -0,0 +1,14 @@
+from Vexera import BOT, UBOT, TBOT, BOT_TOKEN
+
+if __name__ == "__main__":
+    UBOT.start()
+    BOT.run()
+    with BOT:
+        BOT.send_message("-1001768984791", "Pyrogram Client Started")
+        
+    TBOT.start(bot_token=BOT_TOKEN)
+    TBOT.send_message(-1001768984791, "Telethon Client Started") #start message 
+    
+    #ubot is Userbot
+#tbot is Telethon client
+#bot is pyrogram client 
