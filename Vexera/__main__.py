faa5177a82c9767f11032170588d53fee59c3413
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2023-03-17 23:28:42 +0530

    Update __main__.py

--- Vexera/__main__.py
+++ Vexera/__main__.py
@@ -1,10 +1,6 @@
 from Vexera import BOT, UBOT, TBOT, BOT_TOKEN
 
 if __name__ == "__main__":
-    UBOT.start()
-    BOT.run()
-    with BOT:
-        BOT.send_message("-1001768984791", "Pyrogram Client Started")
         
     TBOT.start(bot_token=BOT_TOKEN)
     #start message 
