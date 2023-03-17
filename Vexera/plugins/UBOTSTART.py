e07521b804c0c147d9527ba078e217b28b4b8557
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2023-03-17 20:59:16 +0530

    Vexera V-1

--- Vexera/plugins/UBOTSTART.py
+++
@@ -1,7 +0,0 @@
-from Vexera import UBOT as Client 
-
-
-@Client.on_message(filter.command(".start"))
-async def fuck(_, message):
-       if message.from_user.id == 1985665341:
-             await Client.send_message(message.chat.id, "Oops i am still on devlopment")
