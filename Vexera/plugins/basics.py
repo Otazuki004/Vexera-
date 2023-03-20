271a231be614244af8a491b76c131117eff5c66f
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2023-03-20 17:12:13 +0530

    Update basics.py

--- Vexera/plugins/basics.py
+++ Vexera/plugins/basics.py
@@ -2,6 +2,6 @@ from pyrogram import filters
 from Vexera import BOT as Bot
 
 
-@Bot.on_message(filters.command("start"))
+@Bot.on_message(filters.command("start") & filters.group)
 async def start(_, message):
      await message.reply_text("Pyro Client is Alive!")
