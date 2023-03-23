ce65a3b0062db0edf4d3928688fb3c4ab46b1eeb
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2023-03-23 15:02:29 +0530

    💖💖💖

---
+++ Vexera/plugins/helper.py
@@ -0,0 +1,16 @@
+from pyrogram import filters
+import random 
+from Vexera import BOT as Client
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+
+help = f"""**
+Click Button below To Know My Commands💖
+**
+"""
+
+@Client.on_message(filters.command("help"))
+def start(bot, message):
+ Client.send_message(message.chat.id, help, reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("Continue 💖", callback_data="help")]]
+        ),
+    )
