a9e040036cc1921f5ea8732051f8947b6c0d77ce
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2023-03-23 13:16:16 +0530

    Delete basics.py

--- Vexera/plugins/basics.py
+++
@@ -1,7 +0,0 @@
-from pyrogram import filters
-from Vexera import BOT as Bot
-
-
-@Bot.on_message(filters.command("start") & filters.group)
-async def start(_, message):
-     await message.reply_text("Pyro Client is Alive!")
