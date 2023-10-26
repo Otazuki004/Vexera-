3aca13f703ebe82dbaeca690a9512ed7f9b19538
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-10-26 11:38:55 +0530

    Update bad word remove.py

--- Vexera/plugins/bad word remove.py
+++ Vexera/plugins/bad word remove.py
@@ -1,4 +1,9 @@
 from Vexera import BOT as bot
 from pyrogram.types import *
 import os, io, time
+from Vexera import badword as badd
+
+
+@bot.on_message(filters.command("{badd}"))
+async def delete(_, m):
 
